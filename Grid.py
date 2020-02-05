5c1ffbf0a416d5adb96385723b06ef4c19d955d4
Mateusz <mpotono@gmail.com>
2020-02-05 12:15:37 +0100

    better optimalization

--- Grid.py
+++ Grid.py
@@ -179,7 +179,7 @@ class Grid():
                 for column in range(4):
                     self.H_boundary_global_matrix[nodes_ids[row], nodes_ids[column]] += element.H_boundary_matrix[row, column] 
 
-    def calculate_transform(self, simulation_time=None,simulation_step=None,k=None,c=None,ro=None,alpha=None,t_ambient=None,inital_temperature=None, filename = 'results.txt'):
+    def calculate_transform(self, simulation_time=None,simulation_step=None,k=None,c=None,ro=None,alpha=None,t_ambient=None,inital_temperature=None, filename='results.txt'):
 
         variables = self.read_variables(self.filename)
 
